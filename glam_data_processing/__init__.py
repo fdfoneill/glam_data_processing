5e4c4ec4d54345dae1464c02ffc9a9e519b5a955
fdfoneill <fdfoneill@gmail.com>
2020-10-13 11:55:49 -0400

    Changed where engine.begin() appears in Image.uploadStats()

--- glam_data_processing/__init__.py
+++ glam_data_processing/__init__.py
@@ -2569,27 +2569,32 @@ class Image:
 			"""
 			newCol_val = f"val.{self.doy}"
 			newCol_pct = f"pct.{self.doy}"
-			with self.engine.begin() as connection:
-				try:
+			# with self.engine.begin() as connection:
+			try:
+				with self.engine.begin() as connection:
 					connection.execute(f"SELECT `{newCol_val}` FROM {table_name}") # try to select the desired columns
 					log.debug(f"-column {newCol_val} already exists in table {table_name}")
-				except db.exc.InternalError: # if the column does not exist, the attempt to select it will throw an error
-					# alter the table to add the desired columns
-					aSql = f"ALTER TABLE {table_name} ADD `{newCol_val}` float(2)"
-					try:
-						connection.execute(aSql)
-					except db.exc.InternalError: # the column has somehow appeared between then and now... strange, but it happens with striking regularity on the cluster
-						log.warning(f"Column {newCol_val} has unexpectedly appeared in table {table_name}")
+			except db.exc.InternalError: # if the column does not exist, the attempt to select it will throw an error
+				# alter the table to add the desired columns
+				aSql = f"ALTER TABLE {table_name} ADD `{newCol_val}` float(2)"
 				try:
+					with self.engine.begin() as connection:
+						connection.execute(aSql)
+				except db.exc.InternalError: # the column has somehow appeared between then and now... strange, but it happens with striking regularity on the cluster
+					log.warning(f"Column {newCol_val} has unexpectedly appeared in table {table_name}")
+			try:
+				with self.engine.begin() as connection:
 					connection.execute(f"SELECT `{newCol_pct}` FROM {table_name}") # as above, but for newcol_pct
 					log.debug(f"-column {newCol_pct} already exists in table {table_name}")
-				except db.exc.InternalError:
-					aSql = f"ALTER TABLE {table_name} ADD `{newCol_pct}` float(2)"
-					try:
+			except db.exc.InternalError:
+				aSql = f"ALTER TABLE {table_name} ADD `{newCol_pct}` float(2)"
+				try:
+					with self.engine.begin() as connection:
 						connection.execute(aSql)
-					except db.exc.InternalError: # the column has somehow appeared between then and now... strange, but it happens with striking regularity on the cluster
-						log.warning(f"Column {newCol_val} has unexpectedly appeared in table {table_name}")
-				for index, row in df.iterrows():
+				except db.exc.InternalError: # the column has somehow appeared between then and now... strange, but it happens with striking regularity on the cluster
+					log.warning(f"Column {newCol_val} has unexpectedly appeared in table {table_name}")
+			for index, row in df.iterrows():
+				with self.engine.begin() as connection:
 					uSql= f"UPDATE {table_name} SET `{newCol_val}`={row['value']} WHERE admin = {row['admin']}"
 					connection.execute(uSql)
 					uSql= f"UPDATE {table_name} SET `{newCol_pct}`={row['pct']} WHERE admin = {row['admin']}"
@@ -2643,7 +2648,7 @@ class Image:
 				self.uploadStats(stats_tables)
 			else:
 				log.warning("WARNING: Lost connection to database, 3 retries used up. Skipping.")
-				return "Stats not generated. Database connection lost 4 times in a row. Ouch."
+				return False
 
 		## update product_status if all stats uploaded
 		if admin_level == "ALL" and crop_level == "ALL":
